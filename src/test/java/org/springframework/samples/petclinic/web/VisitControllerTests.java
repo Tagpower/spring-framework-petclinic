52934eee4a24cee90896810846b298a0ab1d472f
Clément Bauchet <clement.bauchet44@gmail.com>
2017-12-12 12:12:46 +0100

    Correction test faux

--- src/test/java/org/springframework/samples/petclinic/web/VisitControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VisitControllerTests.java
@@ -48,7 +48,7 @@ public class VisitControllerTests {
     public void testInitNewVisitForm() throws Exception {
         mockMvc.perform(get("/owners/*/pets/{petId}/visits/new", TEST_PET_ID))
             .andExpect(status().isOk())
-            .andExpect(view().name("pets/createOrUpdateVisitFormblblblblblblblblblbl"));
+            .andExpect(view().name("pets/createOrUpdateVisitForm"));
     }
 
     @Test
